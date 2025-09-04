12c6ae4cc7b374e192adce7580594357e132fefc
SALEEM-MD <s.hacker.pk@gmail.com>
2025-09-04 12:23:06 +0500

    Add files via upload

---
+++ data/index.js
@@ -0,0 +1,40 @@
+// codes by 𝐒𝐀𝐋𝐄𝐄𝐌-𝐌𝐀𝐑𝐖𝐀𝐓
+//CONTACT+923491221845
+const { AntiDelDB,
+    initializeAntiDeleteSettings,
+    setAnti,
+    getAnti,
+    getAllAntiDeleteSettings, } = require('./antidel');
+const {
+    saveContact,
+    loadMessage,
+    getName,
+    getChatSummary,
+    saveGroupMetadata,
+    getGroupMetadata,
+    saveMessageCount,
+    getInactiveGroupMembers,
+    getGroupMembersMessageCount,
+    saveMessage,
+} = require('./store');
+
+
+module.exports = {
+    AntiDelDB,
+    initializeAntiDeleteSettings,
+    setAnti,
+    getAnti,
+    getAllAntiDeleteSettings,
+    saveContact,
+    loadMessage,
+    getName,
+    getChatSummary,
+    saveGroupMetadata,
+    getGroupMetadata,
+    saveMessageCount,
+    getInactiveGroupMembers,
+    getGroupMembersMessageCount,
+    saveMessage,
+};
+// codes by 𝐒𝐀𝐋𝐄𝐄𝐌-𝐌𝐀𝐑𝐖𝐀𝐓
+//CONTACT+923491221845
